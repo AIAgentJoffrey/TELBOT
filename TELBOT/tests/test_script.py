8da61ca64531e614ab53ef3e72016dde5b32dc90
AIAgentJoffrey <aiagent.joffrey@gmail.com>
2025-10-25 03:40:21 +0300

    Delete TELBOT directory

--- TELBOT/tests/test_script.py
+++
@@ -1,9 +0,0 @@
-from bot import make_script
-
-
-def test_make_script():
-    script = make_script("Кафе", "млади професионалисти", "забавен", 20)
-    assert "Кратка видео реклама (20s)" in script
-    assert "Кафе" in script
-    assert "млади професионалисти" in script
-    assert "забавен" in script
